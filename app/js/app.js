f4552528f8d82792afbaaae7391d958fd818ee15
uhero-kito <takayuki.kito@uhero.co.jp>
2015-03-14 13:14:56 +0900

    Place a cursor at the bottom

--- app/js/app.js
+++ app/js/app.js
@@ -6,7 +6,7 @@ function main() {
 
     enchant();
     var core = new Core(DISPLAY_WIDTH, DISPLAY_HEIGHT);
-    core.preload("img/chara1.png");
+    core.preload("img/chara1.png", "img/cursor.png");
     core.fps = 15;
     core.onload = function () {
         var background = (function () {
@@ -36,8 +36,20 @@ function main() {
             sprite.y = (STAGE_HEIGHT / 2) - (height / 2);
             return sprite;
         })();
+        var cursor = (function () {
+            var width = 120;
+            var height = 120;
+            var areaWidth = STAGE_WIDTH;
+            var areaHeight = DISPLAY_HEIGHT - STAGE_HEIGHT;
+            var sprite = new Sprite(width, height);
+            sprite.image = core.assets["img/cursor.png"];
+            sprite.x = (areaWidth / 2) - (width / 2);
+            sprite.y = STAGE_HEIGHT + (areaHeight - height) / 2;
+            return sprite;
+        })();
         core.rootScene.addChild(background);
         core.rootScene.addChild(bear);
+        core.rootScene.addChild(cursor);
     };
     core.start();
 }
