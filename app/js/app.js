a92ce3afbd231c29100b948b60feee6df6c618d4
uhero-kito <takayuki.kito@uhero.co.jp>
2015-03-15 15:23:36 +0900

    Refactor: Add startNewGame()

--- app/js/app.js
+++ app/js/app.js
@@ -7,12 +7,17 @@ function main() {
     var SCORE_TOP = 16;
     var SCORE_TITLE_WIDTH = 48;
 
+    // 左右どちらのボタンが押されているかを管理します
+    var INPUT_NONE = 0;
+    var INPUT_LEFT = 1;
+    var INPUT_RIGHT = 2;
+
     enchant();
     var core = new Core(DISPLAY_WIDTH, DISPLAY_HEIGHT);
     core.preload("img/chara1.png", "img/icon1.png", "img/cursor.png", "img/heart.png");
     core.fps = 15;
     core.onload = function () {
-        var background = (function () {
+        var newBackground = function () {
             var sprite = new Sprite(STAGE_WIDTH, STAGE_HEIGHT);
             sprite.image = (function () {
                 var surface = new Surface(STAGE_WIDTH, STAGE_HEIGHT);
@@ -28,8 +33,8 @@ function main() {
                 return surface;
             })();
             return sprite;
-        })();
-        var bear = (function () {
+        };
+        var newPlayer = function () {
             var width = 32;
             var height = 32;
             var sprite = new Sprite(width, height);
@@ -38,8 +43,8 @@ function main() {
             sprite.x = (STAGE_WIDTH / 2) - (width / 2);
             sprite.y = (STAGE_HEIGHT / 2) - (height / 2);
             return sprite;
-        })();
-        var controller = (function () {
+        };
+        var newController = function () {
             var areaWidth = STAGE_WIDTH;
             var areaHeight = DISPLAY_HEIGHT - STAGE_HEIGHT;
             var sprite = new Sprite(areaWidth, areaHeight);
@@ -52,8 +57,8 @@ function main() {
             sprite.x = 0;
             sprite.y = STAGE_HEIGHT;
             return sprite;
-        })();
-        var cursor = (function () {
+        };
+        var newCursor = function () {
             var width = 120;
             var height = 120;
             var areaWidth = STAGE_WIDTH;
@@ -63,183 +68,188 @@ function main() {
             sprite.x = (areaWidth / 2) - (width / 2);
             sprite.y = STAGE_HEIGHT + (areaHeight - height) / 2;
             return sprite;
-        })();
-        var scoreTitle = (function () {
+        };
+        var newScoreTitle = function () {
             var label = new Label();
             label.text = "Score:";
             label.font = "14px/16px 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
             label.x = SCORE_LEFT;
             label.y = SCORE_TOP;
             return label;
-        })();
-        var scoreNumber = (function () {
+        };
+        var newScoreNumber = function () {
             var label = new Label();
             label.text = "0";
             label.font = "bold 16px/16px 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
             label.x = SCORE_LEFT + SCORE_TITLE_WIDTH;
             label.y = SCORE_TOP;
             return label;
-        })();
+        };
 
-        var gameScene = new Scene();
-        gameScene.addChild(background);
-        gameScene.addChild(bear);
-        gameScene.addChild(controller);
-        gameScene.addChild(cursor);
-        gameScene.addChild(scoreTitle);
-        gameScene.addChild(scoreNumber);
+        var startNewGame = function () {
+            var gameScene = new Scene();
+            var background = newBackground();
+            var bear = newPlayer();
+            var controller = newController();
+            var cursor = newCursor();
+            var scoreTitle = newScoreTitle();
+            var scoreNumber = newScoreNumber();
+            gameScene.addChild(background);
+            gameScene.addChild(bear);
+            gameScene.addChild(controller);
+            gameScene.addChild(cursor);
+            gameScene.addChild(scoreTitle);
+            gameScene.addChild(scoreNumber);
 
-        // 左右どちらのボタンが押されているかを管理します
-        var INPUT_NONE = 0;
-        var INPUT_LEFT = 1;
-        var INPUT_RIGHT = 2;
-        var currentInput = INPUT_NONE;
+            var currentInput = INPUT_NONE;
 
-        // ハートをキャッチするたびに増える得点です
-        var score = 0;
+            // ハートをキャッチするたびに増える得点です
+            var score = 0;
 
-        // ボールがプレイヤーに当たった時に true になります
-        var gameover = false;
+            // ボールがプレイヤーに当たった時に true になります
+            var gameover = false;
 
-        /**
-         * ボタン上をタッチまたはスワイプした際に発火する関数です。
-         * 以下の処理を行います。
-         * 
-         * - ボタンのスプライトを変更し、押された感じを表現します
-         * - プレイヤーのモーションを走っている状態にします
-         * 
-         * @param {Event} e
-         */
-        var inputCursor = function (e) {
-            var input = (e.x < STAGE_WIDTH / 2) ? INPUT_LEFT : INPUT_RIGHT;
-            if (currentInput !== input) {
-                currentInput = input;
-                cursor.frame = [input];
+            /**
+             * ボタン上をタッチまたはスワイプした際に発火する関数です。
+             * 以下の処理を行います。
+             * 
+             * - ボタンのスプライトを変更し、押された感じを表現します
+             * - プレイヤーのモーションを走っている状態にします
+             * 
+             * @param {Event} e
+             */
+            var inputCursor = function (e) {
+                var input = (e.x < STAGE_WIDTH / 2) ? INPUT_LEFT : INPUT_RIGHT;
+                if (currentInput !== input) {
+                    currentInput = input;
+                    cursor.frame = [input];
 
+                    if (!gameover) {
+                        var newScale = (currentInput === INPUT_RIGHT) ? 1 : -1;
+                        bear.scaleX = newScale;
+                        bear.frame = [0, 0, 1, 1, 0, 0, 2, 2];
+                    }
+                }
+            };
+            /**
+             * ボタンから指を離した際に発火する関数です。
+             * 以下の処理を行います。
+             * 
+             * - ボタンのスプライトをデフォルトに変更します
+             * - プレイヤーのモーションを止まっている状態にします
+             * 
+             * @param {Event} e
+             */
+            var stopCursor = function (e) {
+                currentInput = INPUT_NONE;
+                cursor.frame = [INPUT_NONE];
                 if (!gameover) {
-                    var newScale = (currentInput === INPUT_RIGHT) ? 1 : -1;
-                    bear.scaleX = newScale;
-                    bear.frame = [0, 0, 1, 1, 0, 0, 2, 2];
+                    bear.frame = [0];
                 }
-            }
-        };
+            };
+            cursor.addEventListener(Event.TOUCH_START, inputCursor);
+            cursor.addEventListener(Event.TOUCH_MOVE, inputCursor);
+            cursor.addEventListener(Event.TOUCH_END, stopCursor);
 
-        /**
-         * ボタンから指を離した際に発火する関数です。
-         * 以下の処理を行います。
-         * 
-         * - ボタンのスプライトをデフォルトに変更します
-         * - プレイヤーのモーションを止まっている状態にします
-         * 
-         * @param {Event} e
-         */
-        var stopCursor = function (e) {
-            currentInput = INPUT_NONE;
-            cursor.frame = [INPUT_NONE];
-            if (!gameover) {
-                bear.frame = [0];
-            }
-        };
-        cursor.addEventListener(Event.TOUCH_START, inputCursor);
-        cursor.addEventListener(Event.TOUCH_MOVE, inputCursor);
-        cursor.addEventListener(Event.TOUCH_END, stopCursor);
-
-        /**
-         * フレーム毎に実行される関数です。
-         * もしも右か左のボタンが押されていたら、プレイヤーの位置を移動させます。
-         * 
-         * @param {Event} e
-         */
-        var moveBear = function (e) {
-            if (currentInput === INPUT_NONE) {
-                return;
-            }
-
-            var dx = (currentInput === INPUT_LEFT) ? -5 : 5;
-            var newX = Math.min(Math.max(bear.x + dx, 0), STAGE_WIDTH - 32);
-            bear.x = newX;
-        };
-        bear.addEventListener(Event.ENTER_FRAME, moveBear);
-
-        var createHeart = function () {
-            var width = 32;
-            var height = 32;
-            var sprite = new Sprite(width, height);
-            sprite.image = core.assets["img/heart.png"];
-            sprite.x = Math.random() * (STAGE_WIDTH - width);
-            var topToBottom = (Math.random() < 0.5); // true: 上から下, false: 下から上
-            sprite.y = topToBottom ? -width : STAGE_HEIGHT;
-            sprite.frame = [0, 0, 0, 1, 1, 1];
-            sprite.addEventListener(Event.ENTER_FRAME, function (e) {
-                if (gameover) {
+            /**
+             * フレーム毎に実行される関数です。
+             * もしも右か左のボタンが押されていたら、プレイヤーの位置を移動させます。
+             * 
+             * @param {Event} e
+             */
+            var moveBear = function (e) {
+                if (currentInput === INPUT_NONE) {
                     return;
                 }
-                sprite.y += topToBottom ? 3 : -3;
-                var out = (topToBottom && STAGE_HEIGHT < sprite.y) || (!topToBottom && sprite.y < -width);
-                if (out) {
-                    gameScene.removeChild(sprite);
-                }
-                // プレイヤーがハートをキャッチしたら得点を +1 します
-                if (sprite.within(bear, width / 2)) {
-                    gameScene.removeChild(sprite);
-                    score++;
-                    scoreNumber.text = score;
-                }
-            });
-            return sprite;
-        };
-        var createBall = function (e) {
-            var width = 16;
-            var height = 16;
-            var sprite = new Sprite(width, height);
-            sprite.image = core.assets["img/icon1.png"];
-            sprite.x = Math.random() * (STAGE_WIDTH - width);
-            var topToBottom = (Math.random() < 0.5); // true: 上から下, false: 下から上
-            var speed = 2 + (Math.random() * 3);
-            sprite.y = topToBottom ? - width : STAGE_HEIGHT;
-            var frameIndex = (Math.random() < 0.5) ? 0 : 1;
-            sprite.frame = [frameIndex];
-            sprite.addEventListener(Event.ENTER_FRAME, function (e) {
-                sprite.rotate(15);
-                if (gameover) {
+
+                var dx = (currentInput === INPUT_LEFT) ? -5 : 5;
+                var newX = Math.min(Math.max(bear.x + dx, 0), STAGE_WIDTH - 32);
+                bear.x = newX;
+            };
+            bear.addEventListener(Event.ENTER_FRAME, moveBear);
+
+            var createHeart = function () {
+                var width = 32;
+                var height = 32;
+                var sprite = new Sprite(width, height);
+                sprite.image = core.assets["img/heart.png"];
+                sprite.x = Math.random() * (STAGE_WIDTH - width);
+                var topToBottom = (Math.random() < 0.5); // true: 上から下, false: 下から上
+                sprite.y = topToBottom ? -width : STAGE_HEIGHT;
+                sprite.frame = [0, 0, 0, 1, 1, 1];
+                sprite.addEventListener(Event.ENTER_FRAME, function (e) {
+                    if (gameover) {
+                        return;
+                    }
+                    sprite.y += topToBottom ? 3 : -3;
+                    var out = (topToBottom && STAGE_HEIGHT < sprite.y) || (!topToBottom && sprite.y < -width);
+                    if (out) {
+                        gameScene.removeChild(sprite);
+                    }
+                    // プレイヤーがハートをキャッチしたら得点を +1 します
+                    if (sprite.within(bear, width / 2)) {
+                        gameScene.removeChild(sprite);
+                        score++;
+                        scoreNumber.text = score;
+                    }
+                });
+                return sprite;
+            };
+            var createBall = function (e) {
+                var width = 16;
+                var height = 16;
+                var sprite = new Sprite(width, height);
+                sprite.image = core.assets["img/icon1.png"];
+                sprite.x = Math.random() * (STAGE_WIDTH - width);
+                var topToBottom = (Math.random() < 0.5); // true: 上から下, false: 下から上
+                var speed = 2 + (Math.random() * 3);
+                sprite.y = topToBottom ? -width : STAGE_HEIGHT;
+                var frameIndex = (Math.random() < 0.5) ? 0 : 1;
+                sprite.frame = [frameIndex];
+                sprite.addEventListener(Event.ENTER_FRAME, function (e) {
+                    sprite.rotate(15);
+                    if (gameover) {
+                        return;
+                    }
+                    sprite.y += topToBottom ? speed : -speed;
+                    var out = (topToBottom && STAGE_HEIGHT < sprite.y) || (!topToBottom && sprite.y < -width);
+                    if (out) {
+                        gameScene.removeChild(sprite);
+                    }
+                    // ボールがプレイヤーに当たったらゲームオーバーとします
+                    if (sprite.within(bear, width)) {
+                        gameover = true;
+                        bear.frame = [3];
+                        bear.removeEventListener(Event.ENTER_FRAME, moveBear);
+                        bear.tl.moveTo(bear.x, STAGE_HEIGHT + 64, 15, function (t, b, c, d) {
+                            var peak = 0.25;
+                            var x = (t / d) - peak;
+                            return 2 * (x * x * (c - b) + b - (peak * peak));
+                        });
+                        gameScene.removeEventListener(Event.ENTER_FRAME, createObject);
+                    }
+                });
+                return sprite;
+            };
+            var createObject = function (e) {
+                var rand = Math.random();
+                if (0.06 < rand) {
                     return;
                 }
-                sprite.y += topToBottom ? speed : - speed;
-                var out = (topToBottom && STAGE_HEIGHT < sprite.y) || (!topToBottom && sprite.y < - width);
-                if (out) {
-                    gameScene.removeChild(sprite);
-                }
-                // ボールがプレイヤーに当たったらゲームオーバーとします
-                if (sprite.within(bear, width)) {
-                    gameover = true;
-                    bear.frame = [3];
-                    bear.removeEventListener(Event.ENTER_FRAME, moveBear);
-                    bear.tl.moveTo(bear.x, STAGE_HEIGHT + 64, 15, function (t, b, c, d) {
-                        var peak = 0.25;
-                        var x = (t / d) - peak;
-                        return 2 * (x * x * (c - b) + b - (peak * peak));
-                    });
-                    gameScene.removeEventListener(Event.ENTER_FRAME, createObject);
-                }
-            });
-            return sprite;
-        };
-        var createObject = function (e) {
-            var rand = Math.random();
-            if (0.06 < rand) {
-                return;
-            }
 
-            if (0.03 < rand) {
-                var heart = createHeart();
-                gameScene.insertBefore(heart, controller);
-            } else {
-                var ball = createBall();
-                gameScene.insertBefore(ball, controller);
-            }
+                if (0.03 < rand) {
+                    var heart = createHeart();
+                    gameScene.insertBefore(heart, controller);
+                } else {
+                    var ball = createBall();
+                    gameScene.insertBefore(ball, controller);
+                }
+            };
+            gameScene.addEventListener(Event.ENTER_FRAME, createObject);
+            core.replaceScene(gameScene);
         };
-        gameScene.addEventListener(Event.ENTER_FRAME, createObject);
-        core.replaceScene(gameScene);
+
+        startNewGame();
     };
     core.start();
 }
